085586d80597cb063b453544c50e0c82bb095318
NikhilCode12 <promaxchessace@gmail.com>
2024-02-24 01:55:04 +0530

    add: twilio and nodemailer for email otp and mobile otp

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import twilio from "twilio";
+import nodemailer from "nodemailer";
 import authRoutes from "./routes/authRoutes.js";
 import userAccountRoutes from "./routes/userAccountRoutes.js";
 import userWalletRoutes from "./routes/userWalletRoutes.js";
@@ -18,6 +20,21 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
+
+// Initialize Twilio
+const twilioClient = twilio(
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
+);
+
+// Initialize Nodemailer
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
 
 // Middlewares
 app.use(cors());
@@ -48,6 +65,55 @@ mongoose
   .catch((err) => {
     console.log("MongoDB connection failed", err);
   });
+
+// Otp generation
+const generateOTP = () => {
+  return Math.floor(100000 + Math.random() * 900000);
+};
+
+// Send OTP via SMS
+app.post("/api/send-sms-otp", (req, res) => {
+  const { mobileNumber } = req.body;
+  const otp = generateOTP();
+
+  twilioClient.messages
+    .create({
+      body: `Your OTP for verification at fanverse is ${otp}`,
+      from: process.env.TWILIO_PHONE_NUMBER,
+      to: mobileNumber,
+    })
+    .then((message) => {
+      console.log("SMS sent", message.sid);
+      res.json({ msg: "OTP sent successfully" });
+    })
+    .catch((error) => {
+      console.error("Error sending SMS:", error);
+      res.status(500).json({ error: "Failed to send OTP via SMS" });
+    });
+});
+
+// Send OTP via Email
+app.post("/api/send-email-otp", (req, res) => {
+  const { email } = req.body;
+  const otp = generateOTP();
+
+  const mailOptions = {
+    from: process.env.EMAIL,
+    to: email,
+    subject: "Fanverse OTP Verification Code",
+    text: `Your OTP for verification is: ${otp}`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log("Error sending email", error);
+      res.status(500).json({ error: "Failed to send OTP via email" });
+    } else {
+      console.log("Email sent: " + info.response);
+      res.json({ msg: "OTP sent successfully" });
+    }
+  });
+});
 
 // Server
 app.listen(PORT, () => {
