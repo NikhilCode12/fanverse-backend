fa089df31039bdb0e1acc6fe71d53cd8c4f76d85
NikhilCode12 <promaxchessace@gmail.com>
2024-02-23 22:56:57 +0530

    update: updated server with routes middleware

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import authRoutes from "./routes/authRoutes";
+import userAccountRoutes from "./routes/userAccountRoutes";
+import userWalletRoutes from "./routes/userWalletRoutes";
+import matchRoutes from "./routes/matchRoutes";
+import contestRoutes from "./routes/contestRoutes";
+import playerRoutes from "./routes/playerRoutes";
 
 // express app
 const app = express();
@@ -16,6 +22,14 @@ const MONGO_URI = process.env.MONGO_URI;
 // Middlewares
 app.use(cors());
 app.use(bodyParser.json(), bodyParser.urlencoded({ extended: true }));
+
+// Routes Middleware
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userAccountRoutes);
+app.use("/api/wallet", userWalletRoutes);
+app.use("/api/match", matchRoutes);
+app.use("/api/contest", contestRoutes);
+app.use("/api/player", playerRoutes);
 
 // Routes
 app.get("/", (req, res) => {
