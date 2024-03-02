6d295f230865b2880acea8bff3c1c50509668f6b
Aryan-Kohli <106698463+Aryan-Kohli@users.noreply.github.com>
2024-03-02 17:11:51 +0530

    made backend for pre Register

--- routes/userAccountRoutes.js
+++ routes/userAccountRoutes.js
@@ -6,10 +6,12 @@ import {
   getAllUsers,
   updateUserById,
   deleteUserById,
+  findUserByEmailAndRegister
 } from "../controllers/userController.js";
 
 const router = express.Router();
 
+router.post('/findAndCreate',findUserByEmailAndRegister);
 router.post("/create", createUser);
 router.get("/", getUserByToken);
 router.get("/all", getAllUsers);
